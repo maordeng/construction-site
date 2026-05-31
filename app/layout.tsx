1459f4f410ee2678d7a2d585f4fe3e8a0452b5d8
Your Name <you@example.com>
2026-05-31 10:39:37 +0300

    fix favicon conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,6 @@ import Header from "../components/Header";
 export const metadata = {
   title: "מאור דוד הנדסה",
   description: "ניהול ופיקוח בנייה",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({
